d42eb4ba2f890326027006efe75afe2ce41469b2
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-10 21:21:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,12 +45,12 @@ def reduce_to_total(source_array, starting_point=0)
   total = starting_point
   counter = 0 
   while counter < source_array.size do
-    array << source_array[counter] ** 2 
+    total += array[counter] 
     counter += 1 
   end
   total 
 end
-end
+
 
 def reduce_to_all_true(source_array)
 end
