458f8be3ad11c5684bc24624d116c3805c5b465b
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-10 21:19:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,8 +41,8 @@ def map_to_square(source_array)
   array
 end
 
-def reduce_to_total(source_array, starting_point)
-
+def reduce_to_total(source_array, starting_point=0)
+  
 end
 
 def reduce_to_all_true(source_array)
